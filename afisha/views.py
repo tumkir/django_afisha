eccc9048918a8ec6b8c57125dd782080814a3b57
tumkir <tumkir@gmail.com>
2020-07-31 16:38:08 +0300

    Move data from template to view

--- afisha/views.py
+++ afisha/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index_page(request):
-    return render(request, 'index.html')
+    places_geojson = {
+        'type':
+        'FeatureCollection',
+        'features': [{
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.longitude, place.latitude]
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': ''
+            }
+        } for place in Place.objects.all()]
+    }
+
+    data = {'places_geojson': places_geojson}
+    return render(request, 'index.html', context=data)
