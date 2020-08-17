0b7ea0cb4efa5085e3d0052befa35fb058f77277
tumkir <tumkir@gmail.com>
2020-08-18 00:22:03 +0300

    Add TinyMCE for description_long field

--- afisha/urls.py
+++ afisha/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from afisha import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_page),
     path('places/<int:place_id>', views.place_data),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название экскурсии', max_length=200, db_index=True)
     description_short = models.CharField('Короткое описание', max_length=500)
-    description_long = models.TextField('Длинное описание')
+    description_long = HTMLField('Длинное описание')
     longitude = models.DecimalField('Долгота', max_digits=17, decimal_places=14)
     latitude = models.DecimalField('Широта', max_digits=17, decimal_places=14)
 
