67cb64636860a0fee459f6a935f01d3567495f31
tumkir <tumkir@gmail.com>
2020-08-19 02:13:56 +0300

    SECRET_KEY in .env file

--- afisha/settings.py
+++ afisha/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7a6x#eqfue+nr5+lu2tm76q8rb5631g_bbrck^px^$6wzzt36a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
