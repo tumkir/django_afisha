8f7cdaea2b1a3776461ae24139a58e945e068c85
tumkir <tumkir@gmail.com>
2020-10-22 17:45:06 +0300

    description_short from CharField to TextField

--- places/models.py
+++ places/models.py
@@ -4,7 +4,7 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название экскурсии', max_length=200, db_index=True)
-    description_short = models.CharField('Короткое описание', max_length=500)
+    description_short = models.TextField('Короткое описание')
     description_long = HTMLField('Длинное описание')
     longitude = models.DecimalField('Долгота', max_digits=17, decimal_places=14)
     latitude = models.DecimalField('Широта', max_digits=17, decimal_places=14)
